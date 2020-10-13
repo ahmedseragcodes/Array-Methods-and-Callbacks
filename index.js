430ce4bad96f57a61ae3d86172f149e4f0cdb259
ahmedseragcodes <ahmedserag1@gmail.com>
2020-10-13 17:28:39 -0400

    answered task 3 with filter function

--- index.js
+++ index.js
@@ -22,13 +22,11 @@ console.log(filteredFinals);
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(/* code here */) {
+let getYears=fifaData.map(function(game){
+    return game.Year;
+});
+console.log(getYears);
 
-    /* code here */
-
-};
-
-getYears();
 
 /* Task 4: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
 
